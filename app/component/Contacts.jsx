7f4886a0734d4bbf7df8f4daacc70156376fc0ca
AranethWinters <156556665+AranethWinters@users.noreply.github.com>
2024-10-22 22:15:38 +0800

    fixed CSS index error

--- app/component/Contacts.jsx
+++ app/component/Contacts.jsx
@@ -10,27 +10,27 @@ const Contacts = () => {
             <div className='flex flex-col justify-items-center items-center justify-center border-2 border-white p-10 rounded-[2rem]'>
                 <Link href="https://www.youtube.com/@pogeman2345">
                     <div className={`${contactStyles.linked_div}`}>
-                        <FaYoutube size={50} style={"vertical-align: bottom;"}/>
+                        <FaYoutube size={50}/>
                     </div>
                 </Link>
             </div>
             <div className='flex flex-col justify-items-center items-center justify-center border-2 border-white p-10 rounded-[2rem]'>
                 <Link href="https://www.facebook.com/bananaCalara">
                     <div className={`${contactStyles.linked_div}`}>
-                        <FaFacebook size={50} style={"vertical-align: bottom;"}/>
+                        <FaFacebook size={50}/>
                     </div>
                 </Link>
             </div>
             <div className='flex flex-col justify-items-center items-center justify-center border-2 border-white p-10 rounded-[2rem]'>
                 <Link href="https://x.com/nerdyfruit">
                     <div className={`${contactStyles.linked_div}`}>
-                        <FaXTwitter size={50} style={"vertical-align: bottom;"}/>
+                        <FaXTwitter size={50}/>
                     </div>
                 </Link>
             </div>
             <div className='flex flex-col justify-items-center items-center justify-center border-2 border-white p-10 rounded-[2rem]'>
                     <div className={`${contactStyles.linked_div}`}>
-                        <FaEnvelope size={50} style={"vertical-align: bottom;"}/>
+                        <FaEnvelope size={50}/>
                         <p>ernestblane@gmail.com</p>
                     </div>
             </div>
