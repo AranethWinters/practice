f264797308a36efb91c410215a0c83da62661cc8
AranethWinters <156556665+AranethWinters@users.noreply.github.com>
2024-10-22 20:35:52 +0800

    worked on skills page a bit more

--- app/component/Strengths.jsx
+++ app/component/Strengths.jsx
@@ -11,8 +11,24 @@ const Strengths = () => {
         <div className='section_title'>
             <h1 className={`text-center ${myFont.className}`}>Strengths</h1>
         </div>
-        <div>
-            
+        <div className="flex-col gap-4 pb-10 justify-center text-center">
+            <ul className='bg-red inline-block'>
+                <li>
+                    <div className='border-white border-2 rounded-2xl max-w-2xl p-5 m-4'>
+                        <p className='text-center'>Good team communication skills</p>
+                    </div>
+                </li>
+                <li>
+                    <div className='border-white border-2 rounded-2xl max-w-2xl p-5 m-4'>
+                        <p className='text-center'>Trustworthy with projects</p>
+                    </div>
+                </li>
+                <li>
+                    <div className='border-white border-2 rounded-2xl max-w-2xl p-5 m-4'>
+                        <p className='text-center'>Adaptable and teachable</p>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
   )
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './../component/Header.jsx'
 import NavBar from '../component/NavBar.jsx'
 import Footer from '../component/Footer.jsx'
+import AboutMe from '../component/AboutMe.jsx'
 const Contact = () => {
   return (
     <div>
         <NavBar/>
         <Header title="Contact Me"/>
-        <div/>
+        <AboutMe/>
         <Footer/>
     </div>
   )
